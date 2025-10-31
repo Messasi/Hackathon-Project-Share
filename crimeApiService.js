4cb87de4dbb757a4916bc5c288884012587fd285
thinkpad_ubuntu <zeras4z4@gmail.com>
2025-10-31 15:23:59 +0000

    implemented function which requests api, and a wrapper function around it with added months timeframe functionality
    Signed-off-by: thinkpad_ubuntu <zeras4z4@gmail.com>

---
+++ crimeApiService.js
@@ -0,0 +1,34 @@
+async function _fetchCrimesAtLocation(lat, lng, date){
+    const res = await fetch(`https://data.police.uk/api/crimes-at-location?date=${date}&lat=${lat}&lng=${lng}`)
+    const data = await res.json()
+    return data
+}
+
+async function getCrimesAtLocation(lat, lng, crimeTimeframeMonths = 1){
+// assert: crimeTimeframe >= 1
+    
+    const promises = [];
+    const today = new Date();
+    today.setDate(1) // so that the month subtracts correctly
+
+
+    for (let i = 1; i <= crimeTimeframeMonths; i++) {
+        console.log(`i = ${i}`)
+
+        const targetDate = new Date(today);
+        targetDate.setMonth(today.getMonth() - i);
+
+        const year = targetDate.getFullYear();
+        const month = String(targetDate.getMonth() + 1).padStart(2, '0');
+        
+        const dateStr = `${year}-${month}`;
+        console.log(`Requesting data for: ${dateStr}`);
+
+        promises.push(_fetchCrimesAtLocation(lat, lng, dateStr));
+    }
+
+    const results = await Promise.all(promises);
+    console.log(`results = ${results}`)
+
+    return results.flat();
+}
