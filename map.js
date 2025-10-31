149a8b83a9501f99023f93fcc66b0d04501be295
leon <leon@panus.cm>
2025-10-31 15:13:57 +0000

    update map view and increase max zoom level

--- map.js
+++ map.js
@@ -1,5 +1,9 @@
-var map = L.map('map').setView([51.505, -0.09], 13);
+var map = L.map('map').setView([51.505, -0.09], 17);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
+    maxZoom: 40 ,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
+
+//Autofill location 
+
+//Creat a marker
